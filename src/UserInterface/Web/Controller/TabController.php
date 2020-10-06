c24e8d876fb7e8103f225891e88d91069f168438
Daniel Lima <yourwebmaker@gmail.com>
2020-10-06 21:43:11 +0200

    Layout and first route

--- src/UserInterface/Web/Controller/TabController.php
+++ src/UserInterface/Web/Controller/TabController.php
@@ -4,7 +4,17 @@ declare(strict_types=1);
 
 namespace Cafe\UserInterface\Web\Controller;
 
-final class TabController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+final class TabController extends AbstractController
+{
+    /**
+     * @Route(path="tab/open", name="tab_open")
+     */
+    public function open(): Response
+    {
+        return $this->render('tab/open.html.twig');
+    }
 }
