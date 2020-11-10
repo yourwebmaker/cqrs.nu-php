597e676bbab8ccfc803c5e68d296a148d2b4a761
Daniel Lima <yourwebmaker@gmail.com>
2020-11-10 18:16:46 -0300

    Installing Doctrine Migrations

---
+++ migrations/migrations-config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'table_storage' => [
+        'table_name' => 'doctrine_migration_versions',
+        'version_column_name' => 'version',
+        'version_column_length' => 1024,
+        'executed_at_column_name' => 'executed_at',
+        'execution_time_column_name' => 'execution_time',
+    ],
+
+    'migrations_paths' => [
+        'DoctrineMigrations' => './',
+    ],
+
+    'all_or_nothing' => true,
+    'check_database_platform' => true,
+];
