1613376227ef8fee4498e4dac1401934aa47f7df
Daniel Lima <yourwebmaker@gmail.com>
2021-02-13 08:35:28 +0100

    Revert "Use constructor promotion on Domain Events. (#70)"
    This reverts commit f16399e069960587e708f20d1f2e32ce76a30d84.

--- src/Domain/Tab/Events/FoodPrepared.php
+++ src/Domain/Tab/Events/FoodPrepared.php
@@ -8,12 +8,17 @@ use EventSauce\EventSourcing\Serialization\SerializablePayload;
 
 final class FoodPrepared implements SerializablePayload
 {
-    public function __construct(
-        public string $tabId,
-        public string $groupId,
-        /** @var int[] */
-        public array $menuNumbers
-    ) {}
+    public string $tabId;
+    public string $groupId;
+    /** @var int[] */
+    public array $menuNumbers;
+
+    public function __construct(string $tabId, string $groupId, array $menuNumbers)
+    {
+        $this->tabId = $tabId;
+        $this->groupId = $groupId;
+        $this->menuNumbers = $menuNumbers;
+    }
 
     public function toPayload(): array
     {
--- src/Domain/Tab/OrderedItem.php
+++ src/Domain/Tab/OrderedItem.php
@@ -8,12 +8,18 @@ use JsonSerializable;
 
 final class OrderedItem implements JsonSerializable
 {
-    public function __construct(
-        public int $menuNumber,
-        public string $description,
-        public bool $isDrink,
-        public float $price
-    ){}
+    public int $menuNumber;
+    public string $description;
+    public bool $isDrink;
+    public float $price;
+
+    public function __construct(int $menuNumber, string $description, bool $isDrink, float $price)
+    {
+        $this->menuNumber = $menuNumber;
+        $this->description = $description;
+        $this->isDrink = $isDrink;
+        $this->price = $price;
+    }
 
     public function jsonSerialize() : array
     {
