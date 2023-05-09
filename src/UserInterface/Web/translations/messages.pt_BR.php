6328a878393d241671185d94da8b344878a3a013
Daniel Lima <yourwebmaker@gmail.com>
2023-05-09 20:58:54 +0200

    Portuguese translations for home, open and order pages.

---
+++ src/UserInterface/Web/translations/messages.pt_BR.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'home' => [
+        'opened' => 'Comandas abertas'
+    ],
+    'tab' => [
+        'open' => 'Abrir comanda',
+        'waiter' => 'Garçom',
+        'table_number' => 'Número da mesa'
+    ],
+    'order' => [
+        'item_number' => 'Numero item',
+        'item_description' => 'Numero item',
+        'item_quantity' => 'Quantidade',
+        'order' => 'Fazer pedido',
+    ]
+];
