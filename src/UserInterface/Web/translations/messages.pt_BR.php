003fe81985207d74fbd655702a05086044a9ef87
Daniel Lima <yourwebmaker@gmail.com>
2023-05-09 21:21:09 +0200

    Translations for status page.

--- src/UserInterface/Web/translations/messages.pt_BR.php
+++ src/UserInterface/Web/translations/messages.pt_BR.php
@@ -1,17 +1,24 @@
 <?php
 return [
-    'home' => [
-        'opened' => 'Comandas abertas'
-    ],
+    'home' => ['opened' => 'Comandas abertas'],
     'tab' => [
         'open' => 'Abrir comanda',
         'waiter' => 'Garçom',
-        'table_number' => 'Número da mesa'
+        'table_number' => 'Número da mesa',
+        'status' => 'Situação da comanda',
+        'close' => 'Fechar comanda',
+        'for_table' => 'Comanda pra a mesa #%number%',
+    ],
+    'item' => [
+        'to_serve' => 'Items à servir',
+        'served' => 'Servidos',
+        'in_preparation' => 'Items em preparo',
+        'mark_as_served' => 'Marcar como servido',
+        'number' => 'Numero item',
+        'description' => 'Numero item',
+        'quantity' => 'Quantidade',
     ],
     'order' => [
-        'item_number' => 'Numero item',
-        'item_description' => 'Numero item',
-        'item_quantity' => 'Quantidade',
         'order' => 'Fazer pedido',
     ]
 ];
